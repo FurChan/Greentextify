23ff87d7f18fbf133ef411955e7d13be7c381d6b
toothandscale <39612331+toothandscale@users.noreply.github.com>
2018-06-09 20:09:10 -0700

    Create Greentextify.js

---
+++ Greentextify.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Greentextify
+// @require     https://cdnjs.cloudflare.com/ajax/libs/zepto/1.1.4/zepto.min.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Bring greentext everywhere you browse
+// @author       ToothAndScale
+// @match        https://userscripts-mirror.org/scripts/review/287701
+// @grant        none
+// @include      *
+// ==/UserScript==
+
+function applyGreentext(i,s){
+    Zepto(s).wrap('<mark class="greentexted" style="background-color: inherit; color: #789922;"></div>')
+}
+
+function isTextNode(){
+    return this.nodeType===3
+}
+
+function greentextChild(){
+    return Zepto(this).parent().is("mark.greentexted")
+}
+
+function isGreentext(){
+    return Zepto(this).text().trim().substr(0,1)===">"
+}
+
+function greentextTextNodes(){
+    Zepto("*").contents().filter(isTextNode).not(greentextChild).filter(isGreentext).map(applyGreentext)
+}
+
+greentextTextNodes();
